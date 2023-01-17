966d18d72b7006c35fe1762df93aa44396ac0e3c
AoXiang-Soar <2767096139@qq.com>
2023-01-17 23:11:56 +0800

    test2

--- src/main/java/mukaimods/worldgen/WorldGeneratorMusicore.java
+++ src/main/java/mukaimods/worldgen/WorldGeneratorMusicore.java
@@ -10,29 +10,26 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.event.terraingen.OreGenEvent;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
-public class WorldGeneratorMusicore extends WorldGenerator
-{
-	private final WorldGenerator musicoreGenerator = new WorldGenMinable(BlockLoader.blockMusicOre.getDefaultState(), 16);
-    @Override
-    public boolean generate(World world, Random rand, BlockPos pos)
-    {
-    	if (TerrainGen.generateOre(world, rand, this, pos, OreGenEvent.GenerateMinable.EventType.CUSTOM))
-        {
-            for (int i = 0; i < 4; ++i)
-            {
-            	
-                int posX = pos.getX() + rand.nextInt(16);
-                int posY = 16 + rand.nextInt(16);
-                int posZ = pos.getZ() + rand.nextInt(16);
-                BlockPos blockpos = new BlockPos(posX, posY, posZ);
-                musicoreGenerator.generate(world, rand, blockpos);
-               
-            }
-            
-            
-        }
-        return true;
+public class WorldGeneratorMusicore extends WorldGenerator {
+	private final WorldGenerator musicoreGenerator = new WorldGenMinable(BlockLoader.blockMusicOre.getDefaultState(),
+			16);
 
-    }
-    
-    }
+	@Override
+	public boolean generate(World world, Random rand, BlockPos pos) {
+		if (TerrainGen.generateOre(world, rand, this, pos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+			for (int i = 0; i < 4; ++i) {
+
+				int posX = pos.getX() + rand.nextInt(16);
+				int posY = 16 + rand.nextInt(16);
+				int posZ = pos.getZ() + rand.nextInt(16);
+				BlockPos blockpos = new BlockPos(posX, posY, posZ);
+				musicoreGenerator.generate(world, rand, blockpos);
+
+			}
+
+		}
+		return true;
+
+	}
+
+}
